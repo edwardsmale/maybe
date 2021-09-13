3fbe816dd51bd01a5a937e5eb09c4b87cbd3efcc
Ed Smale <ed@paperstone.co.uk>
2021-09-13 20:59:20 +0100

    Initial commit

---
+++ MaybeClass/Maybe.cs
@@ -0,0 +1,196 @@
+﻿using System;
+
+namespace MaybeClass
+{
+    public struct Maybe
+    {
+        // NullClass is a marker type which we can use in one of the implicit conversion operators in Maybe<T>.
+        //
+        // This allows a method that has return type Maybe<T> to do:
+        //
+        //     return Maybe.Null;
+        //
+        public static NullClass Null => new NullClass();
+        public struct NullClass { }
+    }
+
+    public struct Maybe<T>
+    {
+        public Maybe(T value)
+        {
+            this.Value = value;
+            this.NonValue = new MaybeNonValue(MaybeNonValue.NonValueTypeEnum.HasValue);
+        }
+
+        public Maybe(Exception ex = null)
+        {
+            this.Value = default(T);
+
+            if (ex != null)
+            {
+                this.NonValue = new MaybeNonValue(ex);
+            }
+            else
+            {
+                this.NonValue = new MaybeNonValue();
+            }
+        }
+
+        public T Value { get; }
+        private MaybeNonValue NonValue { get; set; }
+
+        public bool HasValue => this.NonValue?.NonValueType == MaybeNonValue.NonValueTypeEnum.HasValue;
+        public bool HasError => !this.HasValue;
+
+        public Exception Exception => this.NonValue.Exception;
+        public string Message => this.NonValue.Message;
+        public bool IsNull => this.NonValue.NonValueType == MaybeNonValue.NonValueTypeEnum.Null;
+
+        // The true and false operators allow a Maybe<T> value to be used in a conditional, e.g.
+        //
+        //     var response = this.GetResponse();  // Returns Maybe<ResponseObject>
+        //
+        //     if (response)
+        //     {
+        //         ...
+        //     }
+        //
+        public static bool operator true(Maybe<T> m) => m.HasValue;
+        public static bool operator false(Maybe<T> m) => !m.HasValue;
+
+        // Allows a Maybe<T> to be passed to a method that takes a T parameter:
+        public static implicit operator T(Maybe<T> m) => m.HasValue ? m.Value : throw new InvalidCastException();
+
+        // Allows a method with return type Maybe<T> to return a T directly:
+        public static implicit operator Maybe<T>(T value) => new Maybe<T>(value);
+
+        // Allows a method with return type Maybe<T> to return an Exception:
+        public static implicit operator Maybe<T>(Exception ex) => new Maybe<T>(ex);
+
+        // Allows a method with return type Maybe<T> to return Maybe.Null:
+        public static implicit operator Maybe<T>(Maybe.NullClass _) => new Maybe<T>() { NonValue = new MaybeNonValue() };
+
+        // Allows comparison between a Maybe<T> and a T.
+        public static bool operator ==(Maybe<T> x, T y) => x.HasValue && x.Value.Equals(y);
+        public static bool operator !=(Maybe<T> x, T y) => !(x == y);
+
+        // Allows comparison between a T and a Maybe<T>.
+        public static bool operator ==(T x, Maybe<T> y) => (y == x);
+        public static bool operator !=(T x, Maybe<T> y) => (y != x);
+
+        // Allows comparison between two Maybe<T> objects by value.
+        public static bool operator ==(Maybe<T> x, Maybe<T> y) => x.Equals(y);
+        public static bool operator !=(Maybe<T> x, Maybe<T> y) => !(x == y);
+
+        public override bool Equals(object obj)
+        {
+            return
+                (obj is Maybe<T> other) &&
+                this.HasValue &&
+                other.HasValue &&
+                this.Value.Equals(other.Value);
+        }
+
+        public override int GetHashCode() => this.GetValueOrNonValue().GetHashCode();
+        public override string ToString() => this.GetValueOrNonValue().ToString();
+
+        private object GetValueOrNonValue()
+        {
+            if (this.HasValue)
+            {
+                return this.Value;
+            }
+            else
+            {
+                return (object)this.NonValue ?? "null";
+            }
+        }
+
+        private class MaybeNonValue
+        {
+            public enum NonValueTypeEnum
+            {
+                /// <summary>
+                /// The Maybe actually has a value.
+                /// </summary>
+                /// <remarks>
+                /// It is more convenient to have this placeholder than to have 
+                /// <see cref="Maybe{T}.NonValue"/> set to null.
+                /// </remarks>
+                HasValue,
+                /// <summary>
+                /// The maybe contains null, rather than a value.
+                /// </summary>
+                Null,
+                /// <summary>
+                /// The maybe contains some kind of message, rather than a value.
+                /// </summary>
+                Message,
+                /// <summary>
+                /// The maybe contains an exception, rather than a value.
+                /// </summary>
+                Exception
+            }
+
+            /// <summary>
+            /// Initialises the object by setting <see cref="NonValueType"/> as specified.
+            /// </summary>
+            /// <remarks>
+            /// Allows the object to be initialised to <see cref="NonValueTypeEnum.HasValue"/>.
+            /// </remarks>
+            public MaybeNonValue(NonValueTypeEnum nonValueType)
+            {
+                this.NonValueType = nonValueType;
+            }
+
+            /// <summary>
+            /// Initialises the object to contain null.
+            /// </summary>
+            public MaybeNonValue()
+            {
+                this.NonValueType = NonValueTypeEnum.Null;
+            }
+
+            /// <summary>
+            /// If <paramref name="message"/> is not null, initialises the object to contain a 
+            /// string message; otherwise initialises the object to contain null.
+            /// </summary>
+            public MaybeNonValue(string message)
+            {
+                if (message == null)
+                {
+                    this.NonValueType = NonValueTypeEnum.Null;
+                }
+                else
+                {
+                    this.Message = message;
+                    this.NonValueType = NonValueTypeEnum.Message;
+                }
+            }
+
+            /// <summary>
+            /// Initialises the object to contain an exception.
+            /// </summary>
+            public MaybeNonValue(Exception exception)
+            {
+                this.Exception = exception;
+                this.NonValueType = NonValueTypeEnum.Exception;
+            }
+
+            /// <summary>
+            /// Gets a value indicating the nature of the data stored in the object.
+            /// </summary>
+            public NonValueTypeEnum NonValueType { get; }
+
+            /// <summary>
+            /// Gets the message that the object contains, if any.
+            /// </summary>
+            public string Message { get; }
+
+            /// <summary>
+            /// Gets the exception that the object contains, if any.
+            /// </summary>
+            public Exception Exception { get; }
+        }
+    }
+}
---
+++ MaybeClass/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace MaybeClass
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            {
+                var maybe = new Maybe<int>(3);
+
+                // A Maybe<T> can be compared to a T for equality:
+
+                Assert(maybe == 3);
+                Assert(3 == maybe);
+            }
+
+            {
+                // A Maybe<T> can be assigned a T value:
+
+                Maybe<int> maybe = 99;
+
+                Assert(maybe == 99);
+            }
+
+            {
+                var maybe = new Maybe<string>("A piece of text");
+
+                // A Maybe<T> can be passed to a method that expects a T:
+
+                var result = MethodThatTakesAString(maybe);
+
+                Assert(result == "A piece of text was passed.");
+            }
+
+            {
+                var maybe = new Maybe<string>(new Exception("It all went wrong!!! :("));
+
+                // A non-valued maybe throws an exception when implicitly converted to T:
+
+                try
+                {
+                    MethodThatTakesAString(maybe);
+                }
+                catch (Exception ex)
+                {
+                    Assert(ex.GetType() == typeof(InvalidCastException));
+                }
+            }
+
+            {
+                // A method with return type Maybe<T> can return a T:
+
+                var maybe = MethodThatReturnsAMaybe(42);
+
+                Assert(maybe == "42 is a great number.");
+            }
+
+            {
+                // A method with return type Maybe<T> can return an exception:
+
+                var maybe = MethodThatReturnsAMaybe(-1);
+
+                Assert(maybe.Exception.GetType() == typeof(ArgumentException));
+            }
+
+            {
+                // A method with return type Maybe<T> can return maybe that contains null.
+
+                var maybe = MethodThatReturnsAMaybe(23);
+
+                Assert(maybe.IsNull);
+            }
+
+            void Assert(bool condition)
+            {
+                if (!condition)
+                {
+                    throw new Exception("Assert failed.");
+                }
+            }
+        }
+
+        public static string MethodThatTakesAString(string str)
+        {
+            return str + " was passed.";
+        }
+
+        public static Maybe<string> MethodThatReturnsAMaybe(int number)
+        {
+            if (number == 42)
+            {
+                return "42 is a great number.";
+            }
+            else if (number < 0)
+            {
+                return new ArgumentException();
+            }
+            else
+            {
+                return Maybe.Null;
+            }
+        }
+    }
+}
